13c1511f3bf332f1aa23d9c9c99f66ab91ab76b5
dsrmurthysoftware <37106923+dsrmurthysoftware@users.noreply.github.com>
2018-04-12 17:54:01 +0530

    Add files via upload

---
+++ lazyloading/src/Blog/Blog.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './Blog.css'
+
+
+class Blog extends Component {
+  
+    render () {
+        return (
+            <div className="container">
+              <h1>Murthy's Blog Page</h1>
+               <h3>mail me at dsrmurthy786@yahoo.com</h3>
+               
+            </div>
+        )
+    }
+}
+
+export default Blog
